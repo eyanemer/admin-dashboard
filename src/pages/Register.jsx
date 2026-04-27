cdfcb18a455c794be5c666505c2ed62a83dab206
eyanemer <108573136+eyanemer@users.noreply.github.com>
2026-04-28 00:57:23 +0100

    Create Register.jsx

---
+++ src/pages/Register.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Register = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <h1 className="text-xl font-bold">Page d'inscription (Bientôt disponible)</h1>
+    </div>
+  );
+};
+
+export default Register;
