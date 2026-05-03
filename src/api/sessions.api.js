5edfb14a502c49f19f75fc306098ab6d8344d25e
eyanemer <108573136+eyanemer@users.noreply.github.com>
2026-05-03 22:48:20 +0100

    Update sessions.api.js

--- src/api/sessions.api.js
+++ src/api/sessions.api.js
@@ -2,3 +2,4 @@ import api from './axios';
 
 export const getSessions = () => api.get('/sessions');
 export const getActiveSessions = () => api.get('/sessions/active');
+export const deleteSession = (id) => api.delete(`/sessions/${id}`);
